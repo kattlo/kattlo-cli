2b24e623e9b4cf647b495c770bc2e9ddc2cf5b12
fabiojose <fabiojose@gmail.com>
2020-10-10 13:08:02 -0300

    Tests to load yaml with create operation
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/main/java/com/github/kattlo/topic/yaml/Loader.java
+++ src/main/java/com/github/kattlo/topic/yaml/Loader.java
@@ -1,12 +1,53 @@
 package com.github.kattlo.topic.yaml;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.util.Map;
 import java.util.regex.Pattern;
+
+import org.yaml.snakeyaml.TypeDescription;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+
+import lombok.Data;
 
 /**
  * @author fabiojose
  */
 public class Loader {
 
+    private static final Constructor CONSTRUCTOR =
+        new Constructor(Model.class);
+
+    private static final TypeDescription DESCRIPTION =
+        new TypeDescription(Model.class);
+
+    static {
+        //DESCRIPTION.addPropertyParameters("config", Map.class);
+        //CONSTRUCTOR.addTypeDescription(DESCRIPTION);
+    };
+
+    private static final Yaml YAML =
+        new Yaml(CONSTRUCTOR);
+
     public static final Pattern FILE_NAME_PATTERN =
         Pattern.compile("v[0-9]{4}_[\\w\\-]{0,246}\\.ya?ml");
+
+    public static Model load(Path file) throws FileNotFoundException {
+
+        return YAML.load(new FileReader(file.toFile()));
+
+    }
+
+    @Data
+    public static class Model {
+        private String operation;
+        private String notes;
+        private String topic;
+        private int partitions;
+        private int replicationFactor;
+
+        private Map<String, Object> config;
+    }
 }
--- src/test/java/com/github/kattlo/topic/yaml/LoaderTest.java
+++ src/test/java/com/github/kattlo/topic/yaml/LoaderTest.java
@@ -1,9 +1,17 @@
 package com.github.kattlo.topic.yaml;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+
+import com.github.kattlo.topic.yaml.Loader.Model;
+
 import org.junit.jupiter.api.Test;
+import org.yaml.snakeyaml.error.YAMLException;
 
 public class LoaderTest {
 
@@ -38,5 +46,40 @@ public class LoaderTest {
         final String fileName = "v0001_my-really-big-long-large-exaustive-apache-kafka-topic-migration-using-kottla-qqqqqqwwwwwweeeeeeeerrrrrrrrrrttttttttttttttttttttyyyyyyyyyyyyyyy-aaaaaaaaaaaaaaaaaaaaapppppppppppppppppppppaaaaaaaaaaaaaaaaaaaacccccccccccccccccccccccchhhhhhhhhhh0000.yaml";
 
         assertTrue(Loader.FILE_NAME_PATTERN.matcher(fileName).matches());
+    }
+
+    @Test
+    public void should_throw_when_migration_file_not_found() {
+
+        final String fileName = "./src/test/resources/topics/file-not-exists.yaml";
+
+        assertThrows(FileNotFoundException.class, () ->
+            Loader.load(Path.of(fileName)));
+    }
+
+    @Test
+    public void should_throw_when_migration_is_an_invalid_yaml() {
+
+        final String fileName = "./src/test/resources/topics/invalid.yaml";
+
+        assertThrows(YAMLException.class, () ->
+            Loader.load(Path.of(fileName)));
+    }
+
+    @Test
+    public void should_load_create_migration() throws Exception {
+
+        final String fileName = "./src/test/resources/topics/create.yaml";
+
+        Model actual =
+            Loader.load(Path.of(fileName));
+
+        assertEquals("create", actual.getOperation());
+        assertEquals("This is a note", actual.getNotes());
+        assertEquals(1, actual.getPartitions());
+        assertEquals(1, actual.getReplicationFactor());
+
+        assertFalse(actual.getConfig().isEmpty());
+        assertEquals(2, actual.getConfig().size());
     }
 }
