6ddc0c693c46e3d55ebd9231aec9f01e9aaae171
fabiojose <fabiojose@gmail.com>
2020-10-10 13:07:17 -0300

    :fire: Rename to TopicOperation
    Tests for yaml with create operation
    
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/main/java/com/github/kattlo/topic/yaml/TopicOperation.java
+++ src/main/java/com/github/kattlo/topic/yaml/TopicOperation.java
@@ -1,9 +1,12 @@
 package com.github.kattlo.topic.yaml;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
+import com.github.kattlo.core.backend.OperationType;
 import com.github.kattlo.util.StringUtil;
 
 import lombok.Builder;
@@ -11,8 +14,9 @@ import lombok.Builder;
 /**
  * @author fabiojose
  */
-public class CreateModel {
+public class TopicOperation {
 
+    private String operation;
     private String notes;
     private String topic;
     private int partitions;
@@ -21,8 +25,18 @@ public class CreateModel {
     private Map<String, Object> config;
 
     @Builder(toBuilder = true)
-    CreateModel(String notes, String topic, int partitions,
+    TopicOperation(String operation, String notes, String topic, int partitions,
         int replicationFactor, Map<String, Object> config){
+
+        this.operation = StringUtil.requireNonBlank(operation);
+        if(!Arrays.asList(OperationType.values()).stream()
+            .map(Enum::name)
+            .map(String::toLowerCase)
+            .collect(Collectors.toList())
+            .contains(this.operation)){
+
+            throw new IllegalArgumentException(operation);
+        }
 
         this.notes = notes;
         this.topic = StringUtil.requireNonBlank(topic);
--- src/test/java/com/github/kattlo/topic/yaml/TopicOperationTest.java
+++ src/test/java/com/github/kattlo/topic/yaml/TopicOperationTest.java
@@ -4,13 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-public class CreateModelTest {
+public class TopicOperationTest {
 
     @Test
     public void should_throw_illegal_when_partitions_is_invalid(){
 
         // setup
-        var create = CreateModel.builder()
+        var create = TopicOperation.builder()
+            .operation("create")
             .notes("notes")
             .topic("topic")
             .partitions(0)
@@ -23,7 +24,8 @@ public class CreateModelTest {
     @Test
     public void should_throw_illegal_when_replication_factor_is_invalid() {
 
-        var create = CreateModel.builder()
+        var create = TopicOperation.builder()
+            .operation("create")
             .notes("notes")
             .topic("topic")
             .partitions(1)
@@ -36,9 +38,38 @@ public class CreateModelTest {
     @Test
     public void should_throw_illegal_when_topic_is_blank() {
 
-        var create = CreateModel.builder()
+        var create = TopicOperation.builder()
+            .operation("create")
             .notes("notes")
             .topic("  ")
+            .partitions(1)
+            .replicationFactor(1)
+            .config(null);
+
+        assertThrows(IllegalArgumentException.class, () -> create.build());
+    }
+
+    @Test
+    public void should_throw_illegal_when_operation_is_blank() {
+
+        var create = TopicOperation.builder()
+            .operation(null)
+            .notes("notes")
+            .topic("topic")
+            .partitions(1)
+            .replicationFactor(1)
+            .config(null);
+
+        assertThrows(IllegalArgumentException.class, () -> create.build());
+    }
+
+    @Test
+    public void should_throw_illegal_when_operation_is_invalid() {
+
+        var create = TopicOperation.builder()
+            .operation("illegal")
+            .notes("notes")
+            .topic("topic")
             .partitions(1)
             .replicationFactor(1)
             .config(null);
