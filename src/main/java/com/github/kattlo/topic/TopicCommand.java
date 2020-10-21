4662cc082139698393144bfaedfb5095e8c1840b
fabiojose <fabiojose@gmail.com>
2020-10-20 21:13:23 -0300

    Tests for one create migration
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/main/java/com/github/kattlo/topic/TopicCommand.java
+++ src/main/java/com/github/kattlo/topic/TopicCommand.java
@@ -2,15 +2,25 @@ package com.github.kattlo.topic;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
 import com.github.kattlo.EntryCommand;
 import com.github.kattlo.core.backend.Backend;
+import com.github.kattlo.core.backend.Migration;
+import com.github.kattlo.core.backend.MigrationToApply;
+import com.github.kattlo.core.backend.ResourceType;
+import com.github.kattlo.core.kafka.Kafka;
+import com.github.kattlo.topic.migration.Strategy;
+import com.github.kattlo.topic.yaml.Loader;
+import com.github.kattlo.topic.yaml.TopicOperation;
+import com.github.kattlo.topic.yaml.TopicOperationMapper;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -25,16 +35,26 @@ import picocli.CommandLine.Model.CommandSpec;
     name = "topic",
     description = "Migrations for Topics"
 )
+@AllArgsConstructor
+@NoArgsConstructor
 public class TopicCommand implements Runnable {
 
+    private static final String NO_VERSION = "v0000";
+
     @ParentCommand
-    private EntryCommand parent;
+    EntryCommand parent;
 
     @Spec
-    private CommandSpec spec;
+    CommandSpec spec;
 
     @Inject
-    private Backend backend;
+    Backend backend;
+
+    @Inject
+    TopicOperationMapper mapper;
+
+    @Inject
+    Kafka kafka;
 
     private File directory;
 
@@ -50,8 +70,8 @@ public class TopicCommand implements Runnable {
         this.directory = Objects.requireNonNull(directory);
     }
 
-    private void validate() {
-        parent.validate();
+    private void validateOptions() {
+        parent.validateOptions();
 
         if(!directory.canRead()){
             throw new CommandLine.
@@ -60,23 +80,71 @@ public class TopicCommand implements Runnable {
         }
     }
 
+    Strategy strategyOf(TopicOperation to){
+        return Strategy.of(to);
+    }
+
     @Override
     public void run() {
-        validate();
+        validateOptions();
 
-        // TODO fetch the 'topic' latest migration
+        try(final var admin = kafka.adminFor(parent.getKafkaConfiguration())) {
 
-        try {
-            Files.list(Paths.get(directory.getAbsolutePath()))
-                .forEach(migration -> {
-                    System.out.println(migration);
+            final var migrationFiles =
+                Loader.list(directory)
+                    .collect(Collectors.toList());
+            for(int i = 0; i < migrationFiles.size(); i++){
+                var migrationFile = migrationFiles.get(i);
 
-                    //TODO Load yaml with migration
+            //for(var migrationFile : migrationFiles) {
+                //TODO Validate yaml against schema??
+                final var migrationModel =
+                    Loader.load(migrationFile);
 
-                    //TODO create, patch or remove?
+                final var operation =
+                    mapper.map(migrationModel, migrationFile);
 
-                    //TODO apply the strategy
+                // fetch the 'topic' latest migration
+                final var latestMigration =
+                    backend.latest(
+                        ResourceType.TOPIC,
+                        operation.getTopic());
+
+                final var currentVersion =
+                    latestMigration
+                        .map(Migration::getApplied)
+                        .map(MigrationToApply::getVersion)
+                        .orElse(NO_VERSION);
+
+                // Remove all migrations related to migrationModel.getTopic()
+                Loader.all(migrationModel.getTopic(),
+                        Path.of(directory.getAbsolutePath()))
+                    .forEach(to -> {
+                        migrationFiles.removeIf(p ->
+                            p.equals(to.getFile()));
+                    });
+
+                // Load newer migrations if any
+                // TODO use the list of all migrations
+                final var newers =
+                    Loader.newer(currentVersion,
+                        migrationModel.getTopic(),
+                        Path.of(directory.getAbsolutePath()));
+
+                newers.forEach(to -> {
+
+                    // create, patch or remove?
+                    final var strategy = strategyOf(to);
+
+                    // apply the strategy
+                    strategy.execute(admin);
+
+                    //TODO Commit the applied migration
+
                 });
+
+
+            }
         }catch(IOException e) {
             throw new CommandLine.ExecutionException(spec.commandLine(),
                 "failing to read migrations: " + e.getMessage());
--- src/test/java/com/github/kattlo/topic/TopicCommandTest.java
+++ src/test/java/com/github/kattlo/topic/TopicCommandTest.java
@@ -1,33 +1,128 @@
 package com.github.kattlo.topic;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.Properties;
+
+import com.github.kattlo.EntryCommand;
+import com.github.kattlo.core.backend.Backend;
+import com.github.kattlo.core.kafka.Kafka;
+import com.github.kattlo.topic.migration.Strategy;
+import com.github.kattlo.topic.yaml.TopicOperation;
+import com.github.kattlo.topic.yaml.TopicOperationMapper;
+
+import org.apache.kafka.clients.admin.AdminClient;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import io.quarkus.test.junit.QuarkusTest;
 import picocli.CommandLine;
+import picocli.CommandLine.Model.CommandSpec;
 
 /**
  * @author fabiojose
  */
-@QuarkusTest
+//@QuarkusTest
+@ExtendWith(MockitoExtension.class)
 public class TopicCommandTest {
 
     private TopicCommand topic = new TopicCommand();
-   
+
+    @Mock
+    EntryCommand parent;
+
+    @Mock
+    CommandSpec spec;
+
+    @Mock
+    Backend backend;
+
+    @Mock
+    Kafka kafka;
+
+    @Mock
+    AdminClient admin;
+
+    @Mock
+    Strategy strategy;
+
+    @Spy
+    TopicOperationMapper mapper =
+        Mappers.getMapper(TopicOperationMapper.class);
+
+    @InjectMocks
+    @Spy
+    TopicCommand command;
+
+    @Captor
+    ArgumentCaptor<TopicOperation> topicOperationCaptor;
+
     @Test
     public void should_exit_code_2_when_directory_not_exists() {
 
         String[] args = {
-            "--config-file=./src/test/java/resources/.bakon.yaml",
+            "--config-file=./src/test/java/resources/.kattlo.yaml",
             "--kafka-cfg=./src/test/java/resources/kafka.properties",
             "topic",
             "--directory=./_not_exists"
         };
 
-        int actual = 
+        int actual =
             new CommandLine(topic).execute(args);
 
         assertEquals(2, actual);
+    }
+
+    @Test
+    public void should_execute_create_strategy() {
+
+        // setup
+        final String topic = "01_try_to_create_topic";
+        final File directory = new File("./src/test/resources/topics/01_try_to_create_topic/");
+
+        //when(parent.getConfiguration())
+        //    .thenReturn(new Properties());
+
+        when(backend.latest(any(), anyString()))
+            .thenReturn(Optional.empty());
+
+        when(kafka.adminFor(any()))
+            .thenReturn(admin);
+
+        //when(command.strategyOf(to))
+        //    .thenReturn(strategy);
+
+        command.setDirectory(directory);
+
+        // act
+        command.run();
+
+        verify(command).strategyOf(topicOperationCaptor.capture());
+        var actual = topicOperationCaptor.getValue();
+
+        // assert
+        assertEquals(topic, actual.getTopic());
+        assertEquals(1, actual.getPartitions());
+        assertEquals(1, actual.getReplicationFactor());
+
+    }
+
+
+    @Test
+    public void should_create_topic() {
+
     }
 }
