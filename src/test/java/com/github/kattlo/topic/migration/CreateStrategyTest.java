95ce200b59b114f99f4d45ada3b9bb9bd8fae45a
fabiojose <fabiojose@gmail.com>
2020-10-22 21:12:17 -0300

    Tests for default partitions, replication factor and configs
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/test/java/com/github/kattlo/topic/migration/CreateStrategyTest.java
+++ src/test/java/com/github/kattlo/topic/migration/CreateStrategyTest.java
@@ -1,12 +1,15 @@
 package com.github.kattlo.topic.migration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyCollection;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.nio.file.Path;
 import java.util.Collection;
+import java.util.Map;
 
 import com.github.kattlo.topic.yaml.TopicOperation;
 
@@ -88,18 +91,136 @@ public class CreateStrategyTest {
     }
 
     @Test
-    public void should_create_with_default_partitions() {
+    public void should_create_with_default_partitions() throws Exception {
 
+        // setup
+        var operation = TopicOperation.builder()
+            .file(Path.of("first"))
+            .version("v0001")
+            .operation("create")
+            .notes("notes")
+            .topic("topic")
+            .replicationFactor(3)
+            .config(null)
+            .build();
+
+        var create = Strategy.of(operation);
+
+        mockitoWhen();
+
+        // act
+        create.execute(admin);
+
+        verify(admin).createTopics(newTopicCaptor.capture());
+        var actual = newTopicCaptor.getValue();
+
+        // assert
+        assertEquals(1, actual.size());
+
+        var newTopic = actual.iterator().next();
+        assertEquals(operation.getTopic(), newTopic.name());
+        assertEquals(-1, newTopic.numPartitions());
+        assertEquals(operation.getReplicationFactor(), newTopic.replicationFactor());
     }
 
     @Test
-    public void should_create_with_default_replication_factor() {
+    public void should_create_with_default_replication_factor() throws Exception {
 
+        // setup
+        var operation = TopicOperation.builder()
+            .file(Path.of("first"))
+            .version("v0001")
+            .operation("create")
+            .notes("notes")
+            .topic("topic")
+            .partitions(3)
+            .config(null)
+            .build();
+
+        var create = Strategy.of(operation);
+
+        mockitoWhen();
+
+        // act
+        create.execute(admin);
+
+        verify(admin).createTopics(newTopicCaptor.capture());
+        var actual = newTopicCaptor.getValue();
+
+        // assert
+        assertEquals(1, actual.size());
+
+        var newTopic = actual.iterator().next();
+        assertEquals(operation.getTopic(), newTopic.name());
+        assertEquals(operation.getPartitions(), newTopic.numPartitions());
+        assertEquals(-1, newTopic.replicationFactor());
     }
 
     @Test
-    public void should_create_with_default_partitions_replication_factor_and_config() {
+    public void should_create_with_default_partitions_replication_factor_and_config() throws Exception {
 
+        // setup
+        var operation = TopicOperation.builder()
+            .file(Path.of("first"))
+            .version("v0001")
+            .operation("create")
+            .notes("notes")
+            .topic("topic")
+            .config(null)
+            .build();
+
+        var create = Strategy.of(operation);
+
+        mockitoWhen();
+
+        // act
+        create.execute(admin);
+
+        verify(admin).createTopics(newTopicCaptor.capture());
+        var actual = newTopicCaptor.getValue();
+
+        // assert
+        assertEquals(1, actual.size());
+
+        var newTopic = actual.iterator().next();
+        assertEquals(operation.getTopic(), newTopic.name());
+        assertEquals(-1, newTopic.numPartitions());
+        assertEquals(-1, newTopic.replicationFactor());
+        assertTrue(newTopic.configs().isEmpty());
+    }
+
+    @Test
+    public void should_create_with_custom_config_and_default_paritions_and_replication_factor() throws Exception {
+
+        // setup
+        var config = Map.of("compression.type", (Object)"snappy");
+        var operation = TopicOperation.builder()
+            .file(Path.of("first"))
+            .version("v0001")
+            .operation("create")
+            .notes("notes")
+            .topic("topic")
+            .config(config)
+            .build();
+
+        var create = Strategy.of(operation);
+
+        mockitoWhen();
+
+        // act
+        create.execute(admin);
+
+        verify(admin).createTopics(newTopicCaptor.capture());
+        var actual = newTopicCaptor.getValue();
+
+        // assert
+        assertEquals(1, actual.size());
+
+        var newTopic = actual.iterator().next();
+        assertEquals(operation.getTopic(), newTopic.name());
+        assertEquals(-1, newTopic.numPartitions());
+        assertEquals(-1, newTopic.replicationFactor());
+        assertEquals("snappy", newTopic.configs().get("compression.type"));
     }
 
     @Test
