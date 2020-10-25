0748f1a6aac4b2c6c2a22d97d1789553e891c2af
fabiojose <fabiojose@gmail.com>
2020-10-25 18:47:24 -0300

    Patch strategy to set new configuration
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/main/java/com/github/kattlo/topic/migration/PatchStrategy.java
+++ src/main/java/com/github/kattlo/topic/migration/PatchStrategy.java
@@ -2,13 +2,21 @@ package com.github.kattlo.topic.migration;
 
 import static org.apache.kafka.clients.admin.NewPartitions.increaseTo;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import com.github.kattlo.topic.yaml.TopicOperation;
 
 import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AlterConfigOp;
+import org.apache.kafka.clients.admin.ConfigEntry;
+import org.apache.kafka.clients.admin.AlterConfigOp.OpType;
+import org.apache.kafka.common.config.ConfigResource;
+import org.apache.kafka.common.config.ConfigResource.Type;
 
 import lombok.AccessLevel;
 import lombok.NonNull;
@@ -53,6 +61,27 @@ public class PatchStrategy implements Strategy {
 
     void patchConfig(AdminClient admin){
 
+        var resource = new ConfigResource(Type.TOPIC, operation.getTopic());
+
+        final Collection<AlterConfigOp> configs =
+        operation.getConfig().entrySet().stream()
+            .map(e -> new ConfigEntry(e.getKey(), e.getValue().toString()))
+            .map(c -> new AlterConfigOp(c, OpType.SET))
+            .collect(Collectors.toCollection(ArrayList::new));
+
+        log.debug("Configurations to change: {}", configs);
+
+        var alter = Map.of(resource, configs);
+        var result = admin.incrementalAlterConfigs(alter);
+
+        try{
+            result.all().get();
+
+            log.debug("Configurations of {} changed to {}",
+                operation.getTopic(), configs);
+        }catch(InterruptedException | ExecutionException e){
+            log.error(e.getMessage(), e);
+        }
     }
 
     @Override
