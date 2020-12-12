d08bda0cc024b0637b39180a479a5ea34bfecfab
fabiojose <fabiojose@gmail.com>
2020-12-12 06:09:35 -0300

    Construct Resource instance from ResourceCommit
    Construct Map from Resource
    
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/main/java/com/github/kattlo/core/backend/Resource.java
+++ src/main/java/com/github/kattlo/core/backend/Resource.java
@@ -3,6 +3,8 @@ package com.github.kattlo.core.backend;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+import com.github.kattlo.core.backend.kafka.ResourceCommit;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,5 +27,35 @@ public class Resource {
     @NonNull private String resourceName;
     @NonNull private LocalDateTime timestamp;
     @NonNull private Map<String, Object> attributes;
+
+    public Map<String, Object> asMap() {
+        return Map.of(
+            "version", version,
+            "status", status,
+            "resourceType", resourceType.name(),
+            "resourceName", resourceName,
+            "timestamp", timestamp,
+            "attributes", Map.copyOf(attributes)
+        );
+    }
+
+    public static Resource from(ResourceCommit commit) {
+        if(null== commit){
+            return null;
+        }
+
+        var resource = new Resource();
+        resource.setVersion(commit.getVersion());
+        resource.setStatus(ResourceStatus.AVAILABLE);
+        if(OperationType.REMOVE.equals(commit.getOperation())){
+            resource.setStatus(ResourceStatus.DELETED);
+        }
+        resource.setResourceType(commit.getResourceType());
+        resource.setResourceName(commit.getResourceName());
+        resource.setTimestamp(commit.getTimestamp());
+        resource.setAttributes(Map.copyOf(commit.getAttributes()));
+
+        return resource;
+    }
 
 }
