6614279bcaadf8d17caebde3cfc9201d626f7db1
fabiojose <fabiojose@gmail.com>
2020-12-12 06:06:36 -0300

    Structure for strong typing of kafka backend records
    Signed-off-by: fabiojose <fabiojose@gmail.com>

---
+++ src/main/java/com/github/kattlo/core/backend/kafka/ResourceCommit.java
@@ -0,0 +1,55 @@
+package com.github.kattlo.core.backend.kafka;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import com.github.kattlo.core.backend.Migration2;
+import com.github.kattlo.core.backend.OperationType;
+import com.github.kattlo.core.backend.ResourceType;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import lombok.Data;
+
+/**
+ * @author fabiojose
+ */
+@RegisterForReflection
+@Data
+public class ResourceCommit {
+
+    private String version;
+
+    private OperationType operation;
+    private String notes;
+    private ResourceType resourceType;
+    private String resourceName;
+    private LocalDateTime timestamp;
+
+    /**
+     * Current attributes state
+     */
+    private Map<String, Object> attributes;
+
+    /**
+     * History of migrated changes
+     */
+    private List<Map<String, Object>> history;
+
+    /**
+     * Creates a copy of migration with empty {@link ResourceCommit#attributes}
+     * and {@link ResourceCommit#history}
+     */
+    public static ResourceCommit from(Migration2 migration){
+
+        var commit = new ResourceCommit();
+        commit.setVersion(migration.getVersion());
+        commit.setOperation(migration.getOperation());
+        commit.setNotes(migration.getNotes());
+        commit.setResourceType(migration.getResourceType());
+        commit.setResourceName(migration.getResourceName());
+        commit.setTimestamp(migration.getTimestamp());
+
+        return commit;
+    }
+}
