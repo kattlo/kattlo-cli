7d2737ee2ecdeffb6fe93c88c8f6f7a489f3637e
fabiojose <fabiojose@gmail.com>
2020-12-16 05:32:21 -0300

    javadocs for author name
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/main/java/com/github/kattlo/core/backend/kafka/ResourceCommitSerializer.java
+++ src/main/java/com/github/kattlo/core/backend/kafka/ResourceCommitSerializer.java
@@ -2,6 +2,9 @@ package com.github.kattlo.core.backend.kafka;
 
 import io.quarkus.kafka.client.serialization.JsonbSerializer;
 
+/**
+ * @author fabiojose
+ */
 public class ResourceCommitSerializer
     extends JsonbSerializer<ResourceCommit> {
 
