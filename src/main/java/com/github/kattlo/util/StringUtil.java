8379a4d1e882bf38b91905216a29262c1bb1a497
fabiojose <fabiojose@gmail.com>
2020-12-30 06:18:39 -0300

    Annotate SringUtil with UtilityClass
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/main/java/com/github/kattlo/util/StringUtil.java
+++ src/main/java/com/github/kattlo/util/StringUtil.java
@@ -2,9 +2,12 @@ package com.github.kattlo.util;
 
 import org.apache.commons.lang3.StringUtils;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * @author fabiojose
  */
+@UtilityClass
 public class StringUtil {
 
     public static String requireNonBlank(String s){
