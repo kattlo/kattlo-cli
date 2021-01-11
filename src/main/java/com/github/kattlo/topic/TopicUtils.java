4070c5982eb3f150366a13762b41d42c99668cb3
fabiojose <fabiojose@gmail.com>
2021-01-11 06:35:06 -0300

    Sniff the exception to check the causa
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/main/java/com/github/kattlo/topic/TopicUtils.java
+++ src/main/java/com/github/kattlo/topic/TopicUtils.java
@@ -9,6 +9,7 @@ import org.apache.kafka.clients.admin.Config;
 import org.apache.kafka.clients.admin.TopicDescription;
 import org.apache.kafka.common.config.ConfigResource;
 import org.apache.kafka.common.config.ConfigResource.Type;
+import org.apache.kafka.common.errors.UnknownTopicOrPartitionException;
 
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
@@ -23,13 +24,24 @@ public final class TopicUtils {
     public static Optional<TopicDescription> describe(String topic, AdminClient admin)
             throws ExecutionException, InterruptedException {
 
-        log.debug("Try to describe the {}", topic);
-        var topicsResult = admin.describeTopics(List.of(topic));
+        try {
 
-        var descriptions = topicsResult.all().get();
-        log.debug("Description of {}: {}", topic, descriptions);
+            log.debug("Try to describe the {}", topic);
+            var topicsResult = admin.describeTopics(List.of(topic));
 
-        return Optional.ofNullable(descriptions.get(topic));
+            var descriptions = topicsResult.all().get();
+            log.debug("Description of {}: {}", topic, descriptions);
+
+            return Optional.ofNullable(descriptions.get(topic));
+
+        }catch(ExecutionException e){
+            if(e.getCause() instanceof UnknownTopicOrPartitionException){
+                return Optional.empty();
+            } else {
+                throw e;
+            }
+        }
+
     }
 
     public static Optional<Config> configsOf(String topic, AdminClient admin)
