e4dec6ff8c000582177c022f3a391631d7804ea6
fabiojose <fabiojose@gmail.com>
2021-01-31 06:37:57 -0300

    In condition impl
    Signed-off-by: fabiojose <fabiojose@gmail.com>

---
+++ src/main/java/com/github/kattlo/core/configuration/rule/In.java
@@ -0,0 +1,27 @@
+package com.github.kattlo.core.configuration.rule;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author fabiojose
+ */
+public class In {
+
+    private final List<Object> operand;
+
+    @SuppressWarnings("unchecked")
+    public In(Object operand) {
+        if(!(operand instanceof List)) {
+            throw new IllegalArgumentException("operand must be an instance of List: " + operand);
+        }
+
+        this.operand = (List<Object>)operand;
+    }
+
+    public boolean execute(Object value) {
+        Objects.requireNonNull(value, "provide a not-null instance for value argument");
+
+        return operand.contains(value);
+    }
+}
---
+++ src/test/java/com/github/kattlo/core/configuration/rule/InTest.java
@@ -0,0 +1,94 @@
+package com.github.kattlo.core.configuration.rule;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class InTest {
+
+    @Test
+    public void should_throw_when_operand_is_not_a_list() {
+
+        assertThrows(IllegalArgumentException.class, () ->
+            new In("operand"));
+    }
+
+    @Test
+    public void should_result_true_when_contains_string() {
+
+        var operand = List.of(
+            "lz4",
+            "snappy"
+        );
+
+        var condition = new In(operand);
+
+        assertTrue(condition.execute("lz4"));
+    }
+
+    @Test
+    public void should_result_true_when_contains_short() {
+
+        var operand = List.of(
+            Short.valueOf("90"),
+            Short.valueOf("92")
+        );
+
+        var condition = new In(operand);
+
+        assertTrue(condition.execute(Short.valueOf("90")));
+    }
+
+    @Test
+    public void should_result_true_when_contains_integer() {
+
+        var operand = List.of(200, 300);
+
+        var condition = new In(operand);
+
+        assertTrue(condition.execute(300));
+    }
+
+    @Test
+    public void should_result_true_when_contains_long() {
+
+        var operand = List.of(200L, 300L);
+
+        var condition = new In(operand);
+
+        assertTrue(condition.execute(300L));
+    }
+
+    @Test
+    public void should_result_true_when_contains_float() {
+
+        var operand = List.of(200.6f, 300.9f);
+
+        var condition = new In(operand);
+
+        assertTrue(condition.execute(300.9f));
+    }
+
+    @Test
+    public void should_result_true_when_contains_double() {
+
+        var operand = List.of(200.6d, 300.9d);
+
+        var condition = new In(operand);
+
+        assertTrue(condition.execute(300.9d));
+    }
+
+    @Test
+    public void should_result_true_when_contains_boolean() {
+
+        var operand = List.of(true, false);
+
+        var condition = new In(operand);
+
+        assertTrue(condition.execute(false));
+    }
+}
