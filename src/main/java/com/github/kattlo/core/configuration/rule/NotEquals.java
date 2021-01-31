4ff8c9edd3ff31839ad5d7ae7097b0d3039ae8ba
fabiojose <fabiojose@gmail.com>
2021-01-31 06:10:19 -0300

    NotEquals condition impl
    Signed-off-by: fabiojose <fabiojose@gmail.com>

---
+++ src/main/java/com/github/kattlo/core/configuration/rule/NotEquals.java
@@ -0,0 +1,18 @@
+package com.github.kattlo.core.configuration.rule;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+
+/**
+ * @author fabiojose
+ */
+@AllArgsConstructor
+public class NotEquals {
+
+    @NonNull
+    private final Object operand;
+
+    public boolean execute(Object value) {
+        return !operand.equals(value);
+    }
+}
---
+++ src/test/java/com/github/kattlo/core/configuration/rule/NotEqualsTest.java
@@ -0,0 +1,63 @@
+package com.github.kattlo.core.configuration.rule;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class NotEqualsTest {
+
+    @Test
+    public void should_result_false_when_string_equals() {
+
+        var value = "CreateTime";
+        var equals = new NotEquals(value);
+
+        assertFalse(equals.execute("CreateTime"));
+    }
+
+    @Test
+    public void should_result_true_when_string_not_equals() {
+
+        var value = "CreateTime";
+        var equals = new NotEquals(value);
+
+        assertTrue(equals.execute("LogAppendTime"));
+    }
+
+    @Test
+    public void should_result_false_when_long_equals() {
+
+        var value = 9223372036854775807L;
+        var equals = new NotEquals(value);
+
+        assertFalse(equals.execute(9223372036854775807L));
+    }
+
+    @Test
+    public void should_result_true_when_long_not_equals() {
+
+        var value = 9223372036854775807L;
+        var equals = new NotEquals(value);
+
+        assertTrue(equals.execute(4096L));
+    }
+
+    @Test
+    public void should_result_false_when_double_equals() {
+
+        var value = 0.0001D;
+        var equals = new NotEquals(value);
+
+        assertFalse(equals.execute(0.0001D));
+    }
+
+    @Test
+    public void should_result_true_when_double_not_equals() {
+
+        var value = 0.0001D;
+        var equals = new NotEquals(value);
+
+        assertTrue(equals.execute(0.5D));
+    }
+}
