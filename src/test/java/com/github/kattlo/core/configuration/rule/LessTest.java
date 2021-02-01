0e4453ddb7d39919edebde3cf32593765780968e
fabiojose <fabiojose@gmail.com>
2021-02-01 06:41:12 -0300

    Refactoring to reduce boilerplate and introduce float and double types
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/test/java/com/github/kattlo/core/configuration/rule/LessTest.java
+++ src/test/java/com/github/kattlo/core/configuration/rule/LessTest.java
@@ -78,6 +78,42 @@ public class LessTest {
     }
 
     @Test
+    public void should_result_true_when_long_float_less() {
+
+        var operand = 92L;
+        var less = new Less(operand);
+
+        assertTrue(less.execute(91f));
+    }
+
+    @Test
+    public void should_result_false_when_long_float_not_less() {
+
+        var operand = 92L;
+        var less = new Less(operand);
+
+        assertFalse(less.execute(92f));
+    }
+
+    @Test
+    public void should_result_true_when_long_double_less() {
+
+        var operand = 92L;
+        var less = new Less(operand);
+
+        assertTrue(less.execute(91D));
+    }
+
+    @Test
+    public void should_result_false_when_long_double_not_less() {
+
+        var operand = 92L;
+        var less = new Less(operand);
+
+        assertFalse(less.execute(92D));
+    }
+
+    @Test
     public void should_result_true_when_int_int_less() {
 
         var operand = 92;
@@ -130,6 +166,42 @@ public class LessTest {
         var less = new Less(operand);
 
         assertFalse(less.execute(Short.valueOf("92")));
+    }
+
+    @Test
+    public void should_result_true_when_int_float_less() {
+
+        var operand = 92;
+        var less = new Less(operand);
+
+        assertTrue(less.execute(91f));
+    }
+
+    @Test
+    public void should_result_false_when_int_float_not_less() {
+
+        var operand = 92;
+        var less = new Less(operand);
+
+        assertFalse(less.execute(92f));
+    }
+
+    @Test
+    public void should_result_true_when_int_double_less() {
+
+        var operand = 92;
+        var less = new Less(operand);
+
+        assertTrue(less.execute(91D));
+    }
+
+    @Test
+    public void should_result_false_when_int_double_not_less() {
+
+        var operand = 92;
+        var less = new Less(operand);
+
+        assertFalse(less.execute(92D));
     }
 
     @Test
@@ -187,5 +259,223 @@ public class LessTest {
 
         assertFalse(less.execute(92L));
 
+    }
+
+    @Test
+    public void should_result_true_when_short_float_less() {
+
+        var operand = Short.valueOf("92");
+        var less = new Less(operand);
+
+        assertTrue(less.execute(91f));
+    }
+
+    @Test
+    public void should_result_false_whe_short_float_less(){
+
+        var operand = Short.valueOf("92");
+        var less = new Less(operand);
+
+        assertFalse(less.execute(92f));
+
+    }
+
+    @Test
+    public void should_result_true_when_short_double_less() {
+
+        var operand = Short.valueOf("92");
+        var less = new Less(operand);
+
+        assertTrue(less.execute(91D));
+    }
+
+    @Test
+    public void should_result_false_when_short_double_not_less(){
+
+        var operand = Short.valueOf("92");
+        var less = new Less(operand);
+
+        assertFalse(less.execute(92D));
+
+    }
+
+    @Test
+    public void should_result_true_when_float_float_less() {
+
+        var operand = 92F;
+        var condition = new Less(operand);
+
+        assertTrue(condition.execute(91F));
+    }
+
+    @Test
+    public void should_result_false_when_float_float_not_less() {
+
+        var operand = 92F;
+        var condition = new Less(operand);
+
+        assertFalse(condition.execute(92F));
+    }
+
+    @Test
+    public void should_result_true_when_float_integer_less() {
+
+        var operand = 92F;
+        var condition = new Less(operand);
+
+        assertTrue(condition.execute(91));
+    }
+
+    @Test
+    public void should_result_false_when_float_integer_not_less() {
+
+        var operand = 92F;
+        var condition = new Less(operand);
+
+        assertFalse(condition.execute(92));
+    }
+
+    @Test
+    public void should_result_true_when_float_long_less() {
+
+        var operand = 92F;
+        var condition = new Less(operand);
+
+        assertTrue(condition.execute(91l));
+    }
+
+    @Test
+    public void should_result_false_when_float_long_not_less() {
+
+        var operand = 92F;
+        var condition = new Less(operand);
+
+        assertFalse(condition.execute(92l));
+    }
+
+    @Test
+    public void should_result_true_when_float_short_less() {
+
+        var operand = 92F;
+        var condition = new Less(operand);
+
+        assertTrue(condition.execute(Short.valueOf("91")));
+    }
+
+    @Test
+    public void should_result_false_when_float_short_not_less() {
+
+        var operand = 92F;
+        var condition = new Less(operand);
+
+        assertFalse(condition.execute(Short.valueOf("92")));
+    }
+
+    @Test
+    public void should_result_true_when_float_double_less() {
+
+        var operand = 92F;
+        var condition = new Less(operand);
+
+        assertTrue(condition.execute(91d));
+    }
+
+    @Test
+    public void should_result_false_when_float_double_not_less() {
+
+        var operand = 92F;
+        var condition = new Less(operand);
+
+        assertFalse(condition.execute(92d));
+    }
+
+    @Test
+    public void should_result_true_when_double_double_less() {
+
+        var operand = 92d;
+        var condition = new Less(operand);
+
+        assertTrue(condition.execute(91d));
+    }
+
+    @Test
+    public void should_result_false_when_double_double_not_less() {
+
+        var operand = 92d;
+        var condition = new Less(operand);
+
+        assertFalse(condition.execute(92d));
+    }
+
+    @Test
+    public void should_result_true_when_double_integer_less() {
+
+        var operand = 92d;
+        var condition = new Less(operand);
+
+        assertTrue(condition.execute(91));
+    }
+
+    @Test
+    public void should_result_false_when_double_integer_not_less() {
+
+        var operand = 92d;
+        var condition = new Less(operand);
+
+        assertFalse(condition.execute(92));
+    }
+
+    @Test
+    public void should_result_true_when_double_long_less() {
+
+        var operand = 92d;
+        var condition = new Less(operand);
+
+        assertTrue(condition.execute(91l));
+    }
+
+    @Test
+    public void should_result_false_when_double_long_not_less() {
+
+        var operand = 92d;
+        var condition = new Less(operand);
+
+        assertFalse(condition.execute(92l));
+    }
+
+    @Test
+    public void should_result_true_when_double_short_less() {
+
+        var operand = 92d;
+        var condition = new Less(operand);
+
+        assertTrue(condition.execute(Short.valueOf("91")));
+    }
+
+    @Test
+    public void should_result_false_when_double_short_not_less() {
+
+        var operand = 92d;
+        var condition = new Less(operand);
+
+        assertFalse(condition.execute(Short.valueOf("92")));
+    }
+
+    @Test
+    public void should_result_true_when_double_float_less() {
+
+        var operand = 92d;
+        var condition = new Less(operand);
+
+        assertTrue(condition.execute(91f));
+    }
+
+    @Test
+    public void should_result_false_when_double_float_not_less() {
+
+        var operand = 92d;
+        var condition = new Less(operand);
+
+        assertFalse(condition.execute(92f));
     }
 }
