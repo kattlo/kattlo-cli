175909fd704719da0c699ce6295b09998cc2a6f7
fabiojose <fabiojose@gmail.com>
2021-02-16 06:09:27 -0300

    Remove unused import from InitCommand.java
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/main/java/com/github/kattlo/InitCommand.java
+++ src/main/java/com/github/kattlo/InitCommand.java
@@ -7,7 +7,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 import java.util.Objects;
 import java.util.Properties;
 
