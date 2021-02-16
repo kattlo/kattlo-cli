66a693f271772e37c7e27fd856571e00db1e9ba1
fabiojose <fabiojose@gmail.com>
2021-02-16 06:08:55 -0300

    Initialize Kattlo project with Topic Rules examples
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/main/java/com/github/kattlo/InitCommand.java
+++ src/main/java/com/github/kattlo/InitCommand.java
@@ -1,12 +1,17 @@
 package com.github.kattlo;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.Objects;
 import java.util.Properties;
 
+import com.github.kattlo.core.exception.WriteException;
 import com.github.kattlo.core.report.PrintStreamReporter;
 import com.github.kattlo.core.report.Reporter;
 import com.github.kattlo.util.VersionUtil;
@@ -35,6 +40,8 @@ public class InitCommand implements Runnable {
 
     private static final String COMMENTS = "Gererated by Kattlo v"
         + VersionUtil.appVersion();
+
+    private static final String KATTLO_FILE_TEMPLATE = "/kattlo.yaml";
 
     static final String KAFKA_FILE = "kafka.properties";
     static final String KATTLO_FILE = ".kattlo.yaml";
@@ -74,6 +81,15 @@ public class InitCommand implements Runnable {
         }
     }
 
+    private void write(String value, FileWriter out) {
+        try{
+            out.write(value);
+            out.write(System.lineSeparator());
+        }catch(IOException e){
+            throw new WriteException(e.getMessage(), e);
+        }
+    }
+
     @Override
     public void run() {
         validateOptions();
@@ -100,15 +116,21 @@ public class InitCommand implements Runnable {
         log.debug("{} written to {}", KAFKA_FILE, kafkaFile);
 
         var kattloFile = new File(getDirectory(), KATTLO_FILE);
-        try{
-            kattloFile.createNewFile();
+        try(var in = getClass().getResourceAsStream(KATTLO_FILE_TEMPLATE);
+            var out = new FileWriter(kattloFile)){
+
+            new BufferedReader(
+                new InputStreamReader(in, StandardCharsets.UTF_8))
+                    .lines()
+                    .forEach(l -> write(l, out));
+
         }catch(IOException e){
             throw new CommandLine.ExecutionException(spec.commandLine(),
                 e.getMessage(), e);
         }
         log.debug("{} written to {}", KATTLO_FILE, kattloFile);
 
-        reporter.initialized(Path.of(getDirectory().toURI()));
+        reporter.initialized(directory.toPath());
     }
 
 }
