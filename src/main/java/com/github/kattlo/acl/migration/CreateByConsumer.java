dbfdad430c5f008db2c5666ec0ef58bc150aac2b
fabiojose <fabiojose@gmail.com>
2021-04-03 06:57:26 -0300

    To acomodate the logic for acl creation by consumer role
    Signed-off-by: fabiojose <fabiojose@gmail.com>

---
+++ src/main/java/com/github/kattlo/acl/migration/CreateByConsumer.java
@@ -0,0 +1,15 @@
+package com.github.kattlo.acl.migration;
+
+import org.apache.kafka.clients.admin.AdminClient;
+
+/**
+ * @author fabiojose
+ */
+public class CreateByConsumer implements Strategy {
+
+    @Override
+    public void execute(AdminClient admin) {
+
+    }
+
+}
