4ecd10d8e197b0987167eab5233b479c34a74119
fabiojose <fabiojose@gmail.com>
2021-04-08 07:09:43 -0300

    Impl at CreateByProducer class the treatment for transacional producer
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/main/java/com/github/kattlo/acl/migration/CreateByProducer.java
+++ src/main/java/com/github/kattlo/acl/migration/CreateByProducer.java
@@ -2,11 +2,20 @@ package com.github.kattlo.acl.migration;
 
 import static com.github.kattlo.acl.migration.CreateStrategy.*;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.github.kattlo.util.JSONPointer;
+import com.github.kattlo.util.JSONUtil;
 
 import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.common.acl.AccessControlEntry;
+import org.apache.kafka.common.acl.AclBinding;
+import org.apache.kafka.common.acl.AclOperation;
+import org.apache.kafka.common.acl.AclPermissionType;
+import org.apache.kafka.common.resource.ResourceType;
 import org.json.JSONObject;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,10 +27,84 @@ import lombok.extern.slf4j.Slf4j;
 public class CreateByProducer implements Strategy {
 
     static final String RELATIVE_POINTER = "#/producer";
+    static final String IDEMPOTENT_RELATIVE_POINTER = "#/idempotent";
+    static final String TRANSACTIONAL_RELATIVE_POINTER = "#/transactional";
 
     private final JSONObject migration;
     CreateByProducer(JSONObject migration){
         this.migration = Objects.requireNonNull(migration);
+    }
+
+    private List<AclBinding> bindingsFor(String principal, JSONObject access,
+        AclPermissionType permission) {
+
+        var producer = JSONPointer.asObject(access, RELATIVE_POINTER).get();
+        var topic = JSONPointer.asObject(producer, CreateByTopic.RELATIVE_POINTER)
+            .map(t -> t.getString("name"))
+            .get();
+        var idempotent = JSONPointer.asBoolean(producer, IDEMPOTENT_RELATIVE_POINTER)
+            .orElseGet(() -> Boolean.FALSE);
+        var transactional = JSONPointer.asObject(producer, TRANSACTIONAL_RELATIVE_POINTER);
+
+        log.debug("Producer topic {}", topic);
+        log.debug("Idempotent Producer {}", idempotent);
+        log.debug("Transactional Producer {}", transactional);
+
+        var pattern = patternOf(topic, ResourceType.TOPIC);
+        log.debug("Topic ResourcePattern {}", pattern);
+
+        var ips = JSONPointer.asArray(access, CONNECTION_RELATIVE_POINTER)
+            .map(JSONUtil::asString)
+            .orElseGet(() -> List.of(WILDCARD_ALL));
+        log.debug("List of IPs to {}: {}", permission, ips);
+
+        var read = ips.stream()
+            .map(ip ->
+                new AccessControlEntry(principal, ip, AclOperation.READ, permission))
+            .map(entry -> new AclBinding(pattern, entry));
+
+        var write = ips.stream()
+            .map(ip ->
+                new AccessControlEntry(principal, ip, AclOperation.WRITE, permission))
+            .map(entry -> new AclBinding(pattern, entry));
+
+        var describe = ips.stream()
+            .map(ip ->
+                new AccessControlEntry(principal, ip, AclOperation.DESCRIBE, permission))
+            .map(entry -> new AclBinding(pattern, entry));
+
+        var idempt = Stream.<AclBinding>of();
+        if(idempotent){
+            idempt = ips.stream()
+                .map(ip ->
+                    new AccessControlEntry(principal, ip, AclOperation.IDEMPOTENT_WRITE, permission))
+                .map(entry -> new AclBinding(pattern, entry));
+        }
+
+        var txPattern = transactional
+            .map(t -> t.getString("id"))
+            .map(id -> patternOf(id, ResourceType.TRANSACTIONAL_ID));
+
+        var txDescribe = txPattern
+            .map(p -> ips.stream()
+                .map(ip -> new AccessControlEntry(principal, ip, AclOperation.DESCRIBE, permission))
+                .map(entry -> new AclBinding(p, entry))
+            )
+            .orElseGet(() -> Stream.of());
+
+        var txWrite = txPattern
+            .map(p -> ips.stream()
+                .map(ip -> new AccessControlEntry(principal, ip, AclOperation.WRITE, permission))
+                .map(entry -> new AclBinding(p, entry))
+            )
+            .orElseGet(() -> Stream.of());
+
+        var result = Stream.concat(Stream.concat(Stream.concat(Stream.concat(Stream.concat(read, write), describe), idempt), txDescribe), txWrite)
+        .collect(Collectors.toList());
+
+        log.debug("By Producer result: {}", result);
+
+        return result;
     }
 
     @Override
@@ -38,11 +121,17 @@ public class CreateByProducer implements Strategy {
 
         scanForRepeatedIP(ipsToAllow, ipsToDeny);
 
-        // TODO create bindings
+        // create bindings
+        var toAllow = allow
+            .map(a -> bindingsFor(principal, a, AclPermissionType.ALLOW))
+            .orElseGet(() -> List.of());
 
-        // TODO scan for repeated
+        var toDeny = deny
+            .map(a -> bindingsFor(principal, a, AclPermissionType.DENY))
+            .orElseGet(() -> List.of());
 
-        // TODO apply
+        // apply
+        apply(toAllow, toDeny, admin);
 
     }
 
