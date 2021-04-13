699e0a718ac0eb2c39bbf7c5fc39281e4352d7e4
fabiojose <fabiojose@gmail.com>
2021-04-13 06:38:50 -0300

    Create ACL by Consumer role
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/main/java/com/github/kattlo/acl/migration/CreateByConsumer.java
+++ src/main/java/com/github/kattlo/acl/migration/CreateByConsumer.java
@@ -1,15 +1,142 @@
 package com.github.kattlo.acl.migration;
 
+import static com.github.kattlo.acl.migration.CreateStrategy.*;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.github.kattlo.util.JSONPointer;
+import com.github.kattlo.util.JSONUtil;
+import com.github.kattlo.util.StringUtil;
+
 import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.common.acl.AccessControlEntry;
+import org.apache.kafka.common.acl.AclBinding;
+import org.apache.kafka.common.acl.AclOperation;
+import org.apache.kafka.common.acl.AclPermissionType;
+import org.apache.kafka.common.resource.ResourceType;
+import org.json.JSONObject;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author fabiojose
  */
+@Slf4j
 public class CreateByConsumer implements Strategy {
+
+    static final String THROW_MSG_TOPIC = "consumer topic and top level topic has the same name %s";
+    static final String RELATIVE_POINTER = "#/consumer";
+    static final String GROUP_RELATIVE_POINTER = "#/group";
+
+    private final JSONObject migration;
+    CreateByConsumer(JSONObject migration){
+        this.migration = Objects.requireNonNull(migration);
+    }
+
+    private List<AclBinding> bindingsFor(String principal, JSONObject access,
+        AclPermissionType permission) {
+
+        var consumer = JSONPointer.asObject(access, RELATIVE_POINTER).get();
+        var topic = JSONPointer.asObject(consumer, CreateByTopic.RELATIVE_POINTER)
+            .map(t -> t.getString(CreateByTopic.NAME_ATTRIBUTE))
+            .get();
+        var group = JSONPointer.asObject(consumer, GROUP_RELATIVE_POINTER)
+            .map(g -> g.getString("id"))
+            .get();
+
+        log.debug("Consumer topic {}", topic);
+        log.debug("Consumer group {}", group);
+
+        var topicPattern = patternOf(topic, ResourceType.TOPIC);
+        log.debug("Topic ResourcePattern {}", topicPattern);
+
+        var groupPattern = patternOf(group, ResourceType.GROUP);
+        log.debug("Group ResourcePattern {}", groupPattern);
+
+        var ips = JSONPointer.asArray(access, CONNECTION_RELATIVE_POINTER)
+            .map(JSONUtil::asString)
+            .orElseGet(() -> List.of(WILDCARD_ALL));
+        log.debug("List of IPs to {}: {}", permission, ips);
+
+        var readTopic = ips.stream()
+            .map(ip ->
+                new AccessControlEntry(principal, ip, AclOperation.READ, permission))
+            .map(a -> new AclBinding(topicPattern, a));
+
+        var describeTopic = ips.stream()
+            .map(ip ->
+                new AccessControlEntry(principal, ip, AclOperation.DESCRIBE, permission))
+            .map(a -> new AclBinding(topicPattern, a));
+
+        var readGroup = ips.stream()
+            .map(ip ->
+                new AccessControlEntry(principal, ip, AclOperation.READ, permission))
+            .map(a -> new AclBinding(groupPattern, a));
+
+        var result = Stream.concat(Stream.concat(readTopic, describeTopic), readGroup)
+            .collect(Collectors.toList());
+        log.debug("By Consumer result {}", result);
+
+        return result;
+    }
+
+    private void scanForRepeatedGroup(Optional<JSONObject> operation) {
+
+        var group = operation
+            .flatMap(o -> JSONPointer.asObject(o, RELATIVE_POINTER))
+            .flatMap(c -> JSONPointer.asObject(c, GROUP_RELATIVE_POINTER))
+            .map(g -> g.getString("id"));
+
+        var toplevelGroup = operation
+            .flatMap(o -> JSONPointer.asObject(o, GROUP_RELATIVE_POINTER))
+            .map(g -> g.getString("id"))
+            .orElseGet(() -> StringUtil.NO_VALUE);
+
+        group.filter(g -> g.equals(toplevelGroup))
+            .ifPresent(g -> {
+                throw new AclCreateException("consumer group and top level one has the same id: " + g);
+            });
+    }
 
     @Override
     public void execute(AdminClient admin) {
 
+        var principal = JSONPointer.asString(migration,
+            PRINCIPAL_ABSOLUTE_POINTER).get();
+
+        var allow = JSONPointer.asObject(migration, ALLOW_ABSOLUTE_POINTER);
+        var deny = JSONPointer.asObject(migration, DENY_ABSOLUTE_POINTER);
+
+        scanForRepeatedTopic(allow, RELATIVE_POINTER).ifPresent(t -> {
+            throw new AclCreateException("allow consumer topic and top level one has the same name: " + t);
+        });
+        scanForRepeatedTopic(deny, RELATIVE_POINTER).ifPresent(t -> {
+            throw new AclCreateException("deny consumer topic and top level one has the same name: " + t);
+        });
+
+        scanForRepeatedGroup(allow);
+        scanForRepeatedGroup(deny);
+
+        var ipsToAllow = connectionIPs(allow);
+        var ipsToDeny = connectionIPs(deny);
+
+        scanForRepeatedIP(ipsToAllow, ipsToDeny);
+
+        // create bindings
+        var toAllow = allow
+            .map(a -> bindingsFor(principal, a, AclPermissionType.ALLOW))
+            .orElseGet(() -> List.of());
+
+        var toDeny = deny
+            .map(a -> bindingsFor(principal, a, AclPermissionType.DENY))
+            .orElseGet(() -> List.of());
+
+        // apply
+        apply(toAllow, toDeny, admin);
     }
 
 }
--- src/test/java/com/github/kattlo/acl/migration/CreateByConsumerTest.java
+++ src/test/java/com/github/kattlo/acl/migration/CreateByConsumerTest.java
@@ -1,39 +1,213 @@
 package com.github.kattlo.acl.migration;
 
+import static org.mockito.ArgumentMatchers.anyCollection;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.nio.file.Path;
+import java.util.Collection;
+
+import com.github.kattlo.acl.yaml.Loader;
+import com.github.kattlo.core.yaml.MigrationLoader;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.CreateAclsResult;
+import org.apache.kafka.common.KafkaFuture;
+import org.apache.kafka.common.acl.AclBinding;
+import org.apache.kafka.common.acl.AclOperation;
+import org.apache.kafka.common.acl.AclPermissionType;
+import org.apache.kafka.common.resource.PatternType;
+import org.apache.kafka.common.resource.ResourceType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class CreateByConsumerTest {
 
-    @Test
-    public void should_create_topic_with_allow_consumer() {
+    @Mock
+    AdminClient admin;
+
+    @Mock
+    CreateAclsResult result;
+
+    @Mock
+    KafkaFuture<Void> future;
+
+    @Captor
+    private ArgumentCaptor<Collection<AclBinding>> newACLCaptor;
+
+    private void mockitoWhen() throws Exception {
+
+        when(admin.createAcls(anyCollection()))
+            .thenReturn(result);
+
+        when(result.all())
+            .thenReturn(future);
+
+        when(future.get())
+            .thenReturn((Void)null);
 
     }
 
     @Test
-    public void should_throw_when_allow_consumer_and_top_level_topic_has_same_value() {
+    @SuppressWarnings("unchecked")
+    public void should_create_topic_with_allow_consumer() throws Exception {
 
+        var yaml = Path.of("./src/test/resources/acl/by-principal/v0001_create-consumer-allow.yaml");
+        var map = Loader.loadAsMap(yaml);
+
+        var migration = MigrationLoader.parseJson(map);
+
+        var strategy = Strategy.of(migration);
+
+        mockitoWhen();
+
+        // act
+        strategy.execute(admin);
+
+        // assert
+        verify(admin).createAcls(newACLCaptor.capture());
+        var actual = newACLCaptor.getValue();
+
+        assertThat(actual, allOf(
+            hasSize(3)
+        ));
+
+        var acls = actual.iterator();
+        var acl0 = acls.next();
+        assertEquals(AclPermissionType.ALLOW, acl0.entry().permissionType());
+        assertEquals(AclOperation.READ, acl0.entry().operation());
+        assertEquals("*", acl0.entry().host());
+        assertEquals("User:CN=Alice,OU=Sales,O=Unknown,L=Unknown,ST=SP,C=Unknown",
+            acl0.entry().principal());
+        assertEquals(ResourceType.TOPIC, acl0.pattern().resourceType());
+        assertEquals(PatternType.LITERAL, acl0.pattern().patternType());
+        assertEquals("topic-as-consumer", acl0.pattern().name());
+
+        var acl1 = acls.next();
+        assertEquals(AclPermissionType.ALLOW, acl1.entry().permissionType());
+        assertEquals(AclOperation.DESCRIBE, acl1.entry().operation());
+        assertEquals("*", acl1.entry().host());
+        assertEquals("User:CN=Alice,OU=Sales,O=Unknown,L=Unknown,ST=SP,C=Unknown",
+            acl1.entry().principal());
+        assertEquals(ResourceType.TOPIC, acl1.pattern().resourceType());
+        assertEquals(PatternType.LITERAL, acl1.pattern().patternType());
+        assertEquals("topic-as-consumer", acl1.pattern().name());
+
+        var acl2 = acls.next();
+        assertEquals(AclPermissionType.ALLOW, acl2.entry().permissionType());
+        assertEquals(AclOperation.READ, acl2.entry().operation());
+        assertEquals("*", acl2.entry().host());
+        assertEquals("User:CN=Alice,OU=Sales,O=Unknown,L=Unknown,ST=SP,C=Unknown",
+            acl2.entry().principal());
+        assertEquals(ResourceType.GROUP, acl2.pattern().resourceType());
+        assertEquals(PatternType.LITERAL, acl2.pattern().patternType());
+        assertEquals("group.id-as-consumer", acl2.pattern().name());
+
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void should_create_topic_with_deny_consumer() throws Exception {
+
+        var yaml = Path.of("./src/test/resources/acl/by-principal/v0001_create-consumer-deny.yaml");
+        var map = Loader.loadAsMap(yaml);
+
+        var migration = MigrationLoader.parseJson(map);
+
+        var strategy = Strategy.of(migration);
+
+        mockitoWhen();
+
+        // act
+        strategy.execute(admin);
+
+        // assert
+        verify(admin).createAcls(newACLCaptor.capture());
+        var actual = newACLCaptor.getValue();
+
+        assertThat(actual, allOf(
+            hasSize(3)
+        ));
+
+        var acls = actual.iterator();
+        var acl0 = acls.next();
+        assertEquals(AclPermissionType.DENY, acl0.entry().permissionType());
+        assertEquals(AclOperation.READ, acl0.entry().operation());
+        assertEquals("172.16.0.1", acl0.entry().host());
+        assertEquals("User:CN=Alice,OU=Sales,O=Unknown,L=Unknown,ST=SP,C=Unknown",
+            acl0.entry().principal());
+        assertEquals(ResourceType.TOPIC, acl0.pattern().resourceType());
+        assertEquals(PatternType.LITERAL, acl0.pattern().patternType());
+        assertEquals("topic-as-consumer", acl0.pattern().name());
+
+        var acl1 = acls.next();
+        assertEquals(AclPermissionType.DENY, acl1.entry().permissionType());
+        assertEquals(AclOperation.DESCRIBE, acl1.entry().operation());
+        assertEquals("172.16.0.1", acl1.entry().host());
+        assertEquals("User:CN=Alice,OU=Sales,O=Unknown,L=Unknown,ST=SP,C=Unknown",
+            acl1.entry().principal());
+        assertEquals(ResourceType.TOPIC, acl1.pattern().resourceType());
+        assertEquals(PatternType.LITERAL, acl1.pattern().patternType());
+        assertEquals("topic-as-consumer", acl1.pattern().name());
+
+        var acl2 = acls.next();
+        assertEquals(AclPermissionType.DENY, acl2.entry().permissionType());
+        assertEquals(AclOperation.READ, acl2.entry().operation());
+        assertEquals("172.16.0.1", acl2.entry().host());
+        assertEquals("User:CN=Alice,OU=Sales,O=Unknown,L=Unknown,ST=SP,C=Unknown",
+            acl2.entry().principal());
+        assertEquals(ResourceType.GROUP, acl2.pattern().resourceType());
+        assertEquals(PatternType.LITERAL, acl2.pattern().patternType());
+        assertEquals("group.id-as-consumer", acl2.pattern().name());
+    }
+
+    @Test
+    public void should_throw_when_allow_consumer_and_top_level_topic_has_same_value() throws Exception {
+
+        var yaml = Path.of("./src/test/resources/acl/by-principal/v0001_create-consumer-allow-same-topic.yaml");
+        var map = Loader.loadAsMap(yaml);
+
+        var migration = MigrationLoader.parseJson(map);
+
+        var strategy = Strategy.of(migration);
+
+        // for create by topic strategy
+        mockitoWhen();
+
+        //assert
+        var actual = assertThrows(AclCreateException.class, () ->
+            // act
+            strategy.execute(admin));
+
+        assertThat(actual.getMessage(),
+            containsString("consumer topic and top level one has the same name"));
     }
 
     @Test
     public void should_throw_when_allow_consumer_and_top_level_group_has_same_value() {
 
-    }
+        var yaml = Path.of("./src/test/resources/acl/by-principal/v0001_create-consumer-allow-same-group.yaml");
+        var map = Loader.loadAsMap(yaml);
 
-    @Test
-    public void should_create_topic_with_deny_consumer() {
+        var migration = MigrationLoader.parseJson(map);
 
-    }
+        var strategy = Strategy.of(migration);
 
-    @Test
-    public void should_throw_when_deny_consumer_and_top_level_topic_has_same_value() {
+        //assert
+        var actual = assertThrows(AclCreateException.class, () ->
+            // act
+            strategy.execute(admin));
 
-    }
-
-    @Test
-    public void should_throw_when_deny_consumer_and_top_level_group_has_same_value() {
-
+        assertThat(actual.getMessage(),
+            containsString("consumer group and top level one has the same id"));
     }
 }
