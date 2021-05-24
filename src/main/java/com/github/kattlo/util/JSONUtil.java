2ef39c084fbe697b4fa96892bb07b7753909128e
fabiojose <fabiojose@gmail.com>
2021-05-24 06:24:25 -0300

    Methods to transform JSONObject and JSONArray to Map and List in JSONUtil class
    Signed-off-by: fabiojose <fabiojose@gmail.com>

--- src/main/java/com/github/kattlo/util/JSONUtil.java
+++ src/main/java/com/github/kattlo/util/JSONUtil.java
@@ -1,7 +1,9 @@
 package com.github.kattlo.util;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
@@ -47,4 +49,47 @@ public class JSONUtil {
                     .jsonProvider(getJsonProvider()));
     }
 
+    /* thanks: https://stackoverflow.com/questions/41243880/how-to-convert-jsonobject-to-new-map-for-all-its-keys-using-iterator-java
+     */
+    public static Map<String, Object> toMap(JSONObject o) {
+        var map = new HashMap<String, Object>();
+
+        var keysItr = o.keys();
+        while(keysItr.hasNext()) {
+            var key = keysItr.next();
+            var value = o.get(key);
+
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+
+            }else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
+    /* thanks: https://stackoverflow.com/questions/41243880/how-to-convert-jsonobject-to-new-map-for-all-its-keys-using-iterator-java
+     */
+    public static List<Object> toList(JSONArray a) {
+
+        var list = new ArrayList<Object>();
+        for(int i = 0; i < a.length(); i++) {
+            var value = a.get(i);
+
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            list.add(value);
+        }
+
+        return list;
+    }
 }
